db634831895300a8e97c7dab38e80aa233ea69f5
Sawacha Emmanuel <sawachaemmanuel@gmail.com>
2025-09-16 08:47:12 +0100

    first commit

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -62,8 +62,8 @@ const Auth = () => {
           <CardContent>
             <Tabs defaultValue="signin" className="w-full">
               <TabsList className="grid w-full grid-cols-2">
-                <TabsTrigger value="signin">Sign In</TabsTrigger>
-                <TabsTrigger value="signup">Sign Up</TabsTrigger>
+                {/* <TabsTrigger value="signin">Sign In</TabsTrigger>
+                <TabsTrigger value="signup">Sign Up</TabsTrigger> */}
               </TabsList>
               
               <TabsContent value="signin">
